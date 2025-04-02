b6c9bb5a77c33156aff01719d89b2d0b0a0f2a88
Kovalenko <amidalo148@gmail.com>
2025-04-03 04:02:28 +0700

    Add TK-10 (valid email && invalid password)

--- src/test/java/AuthorizationTest.java
+++ src/test/java/AuthorizationTest.java
@@ -47,8 +47,9 @@ public class AuthorizationTest {
         };
     }
 
-    @Test(dataProvider = "Недействительные Email",
-            description = "Проверка сообщений об ошибках при авторизации")
+    // ТК-9
+    @Test(dataProvider = "Недействительные Email", enabled = false, priority = 1,
+            description = "Проверка сообщений об ошибках авторизации при невалидных email")
     public void testLoginErrors(String email, String password, String expectedMessage) {
         webDriver.get("https://same-v739m8ov3qm-latest.netlify.app/login");
         webDriver.manage().window().maximize();
@@ -75,6 +76,60 @@ public class AuthorizationTest {
 
             By errorLocator = By.xpath("//p[contains(text()," +
                     " 'Пожалуйста, введите корректный адрес электронной почты')]");
+            // Проверка сообщения
+            WebElement errorMessage = webDriver.findElement(errorLocator);
+            Assert.assertEquals(errorMessage.getText(), expectedMessage);
+
+        } catch (InterruptedException e) {
+            Assert.fail("Тест был прерван: " + e.getMessage());
+        } catch (NoSuchElementException e) {
+            Assert.fail("Возможно сообщение c предупреждением еще не успело появиться: " + e.getMessage() +
+                    "\n\nПопробуйте увеличить время паузы.");
+        }
+    }
+
+    @DataProvider(name = "Недействительные пароли")
+    public Object[][] providePasswordData() {
+        return new Object[][] {
+                // email,                          password,            expectedMessage
+                {"user@gmail.com",                 "12345",         "Пароль должен содержать минимум 6 символов"},
+                {"test@mail.ru",                   "qwert",         "Пароль должен содержать минимум 6 символов"},
+                {"example@yandex.ru",              "        ",      "Некорректный символ в пароле: пробел, кавычки, двоеточие"},
+                {"admin@domain.com",               "1",             "Пароль должен содержать минимум 6 символов"},
+                {"support@company.net",            "",              "Пароль должен содержать минимум 6 символов"},
+                {"support@company.net",            "sdferr:",       "Некорректный символ в пароле: пробел, кавычки, двоеточие"},
+                {"support@company.net",            "'sdferr'",      "Некорректный символ в пароле: пробел, кавычки, двоеточие"},
+        };
+    }
+
+    // ТК-10
+    @Test(dataProvider = "Недействительные пароли", enabled = true, priority = 1,
+            description = "Проверка сообщений об ошибках авторизации при невалидном пароле")
+    public void testPasswordErrors(String email, String password, String expectedMessage) {
+        webDriver.get("https://same-v739m8ov3qm-latest.netlify.app/login");
+        webDriver.manage().window().maximize();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+
+            // 1 действие - вводим нашу почту
+            WebElement emailField = webDriver.findElement(By.xpath("//div//input[@name=\"email\"]"));
+            emailField.sendKeys(email);
+            TimeUnit.SECONDS.sleep(1);
+
+            // 2 действие - вводим наш пароль
+            WebElement passwordField = webDriver.findElement(By.xpath("//div//input[@name=\"password\"]"));
+            passwordField.sendKeys(password);
+            TimeUnit.SECONDS.sleep(1);
+
+            // 3 действие - нажимаем по кнопке 'Войти' на странице входа
+            WebElement submitButton = webDriver.findElement(
+                    By.xpath("//div//button[@type=\"submit\"]")
+            );
+            submitButton.click();
+            TimeUnit.SECONDS.sleep(3);
+
+            By errorLocator = By.xpath("//p[contains(text(), 'Пароль должен содержать минимум 6 символов')]");
             // Проверка сообщения
             WebElement errorMessage = webDriver.findElement(errorLocator);
             Assert.assertEquals(errorMessage.getText(), expectedMessage);
